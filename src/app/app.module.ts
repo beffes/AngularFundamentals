d4f50006fef496b8e472f7f1bf7f773cad19eb50
Bruno Ferreira <beffes@gmail.com>
2019-12-12 10:21:35 +0000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NavBarComponent } from './nav/navbar.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'events-app',
+  template: `
+    <nav-bar></nav-bar>
+    <events-list></events-list>
+  `
+})
+export class EventsAppComponent {
+  title = 'app';
+}
