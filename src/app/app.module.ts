d31746aeb7bb46f409bc6c2cedabeb2cb94d95a9
Bruno Ferreira <beffes@gmail.com>
2019-12-16 16:15:57 +0000

    Routes + Guardian Activator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,30 @@ import { NavBarComponent } from './nav/navbar.component';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
   // tslint:disable-next-line: component-selector
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverweel thumbnail">
+    <div [routerLink]="['/events', event.id]" class="well hoverweel thumbnail">
       <h2> {{event?.name}}</h2>
       <div> Date: {{event?.date}}</div>
       <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
@@ -36,7 +36,7 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
 export class EventThumbnailComponent {
  @Input() event: any;
 
- getStartTimeClass(){
+ getStartTimeClass() {
    if (this.event && this.event.time === '8:00 am') {
     return ['green', 'bold'];
    }
@@ -44,7 +44,7 @@ export class EventThumbnailComponent {
    return [];
  }
 
- getStartTimeStyle(){
+ getStartTimeStyle() {
   if (this.event && this.event.time === '8:00 am') {
    return {color: '#003300', 'font-weight': 'bold'};
   }
